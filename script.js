4f6c977d2be973968d190066d3b58f6dcecb36bb
Kieran Wake <kieranwake1234@gmail.com>
2024-02-12 11:46:08 +0000

    entitled our variables in a new array, which is equal to the original array

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // "eyes", "nose", and "ears" should not be assigned to anything
 
 const arr = ["eyes", "nose", "lips", "ears"]
-let [/* Create variable lips here and assign it to arr[2] */] = arr
+let [a, b, c, d] = arr
 
 //The purpose of ES6's destructuring assignment is to index an arrays -
 //items on one line '[]' instead of having to declare variables for each-
