b1097ea7a25c13e773645d35b7c17ae9a35743bb
Kieran Wake <kieranwake1234@gmail.com>
2024-02-12 11:56:05 +0000

    applying two empty commas in the new array list. proceeded by the remaining elements, will skip over the first couple of indexs when printing to browser

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 // DO NOT change arr
 // DO NOT USE var [lips] = arr[2]
 // "eyes", "nose", and "ears" should not be assigned to anything
-function destructuringArray () {
     const arr = ["eyes", "nose", "lips", "ears"];
-    let [a, b, c, d] = arr;
-    
-    console.log(arr(a, b, d, ...c));
-}
+    let [, , lips, ears] = arr;
+
+    console.log(lips);
  
 
 //The purpose of ES6's destructuring assignment is to index an arrays -
