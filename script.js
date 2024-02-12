6ac063fdf27f74786b81646b0c753e4117040af6
Kieran Wake <kieranwake1234@gmail.com>
2024-02-12 11:48:59 +0000

    placed our array inside a caller function

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // DO NOT change arr
 // DO NOT USE var [lips] = arr[2]
 // "eyes", "nose", and "ears" should not be assigned to anything
-
-const arr = ["eyes", "nose", "lips", "ears"]
-let [a, b, c, d] = arr
+function destructuringArray () {
+    const arr = ["eyes", "nose", "lips", "ears"];
+    let [a, b, c, d] = arr;
+    
+    console.log(arr(a, b, d, ...c));
+}
+ 
 
 //The purpose of ES6's destructuring assignment is to index an arrays -
 //items on one line '[]' instead of having to declare variables for each-
